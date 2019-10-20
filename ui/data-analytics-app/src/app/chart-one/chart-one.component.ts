bf345345646b1fc18ff40f5a5412fd2bfc424487
Suryansh <suryanshsrivastava9616@gmail.com>
2019-10-20 15:49:35 +0530

    Add Chart plugin

--- ui/data-analytics-app/src/app/chart-one/chart-one.component.ts
+++ ui/data-analytics-app/src/app/chart-one/chart-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../canvasjs.min';
 
 @Component({
   selector: 'app-chart-one',
@@ -10,6 +11,29 @@ export class ChartOneComponent implements OnInit {
   constructor() { }
   
   ngOnInit() {
+		let chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+      
+    chart.render();
   }
 
 }
